8999b28006f37d43b0b643744024ae4c44b729ae
marcoleung052 <marcoleung.cgu@gmail.com>
2025-03-20 15:41:36 +0800

    Create page35.c

---
+++ ex3/page35.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char v='A';
+    int* p = null;
+    p=&v;
+    printf("Address of v: %x\n",v);
+    printf("Address stored in p: %x\n",p);
+    printf("Address of p: %x\n",&p);
+    printf("Value of *p: %d\n",*p);
+    return 0;
+}
