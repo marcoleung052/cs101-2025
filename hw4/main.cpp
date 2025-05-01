e83440202a01f49833a824190358b41348bb7bad
marcoleung052 <marcoleung.cgu@gmail.com>
2025-05-01 15:07:35 +0800

    Update main.cpp

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -3,39 +3,48 @@ using namespace std;
 
 class TicTacToe {
 private:
-    char board[3][3];
-    char current_player;
+    int board[3][3]; // 0 = 空, 1 = 玩家1, 2 = 玩家2
+    int current_player;
 
 public:
     TicTacToe() {
         reset_board();
-        current_player = 'X';
+        current_player = 1;
     }
 
     void reset_board() {
-        char pos = '1';
+        int pos = 1;
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
-                board[i][j] = pos++;
+                board[i][j] = 0;
     }
 
     void display_board() {
         cout << "\n";
         for (int i = 0; i < 3; ++i) {
-            cout << " " << board[i][0] << " | " << board[i][1] << " | " << board[i][2] << "\n";
+            for (int j = 0; j < 3; ++j) {
+                if (board[i][j] == 1)
+                    cout << " X ";
+                else if (board[i][j] == 2)
+                    cout << " O ";
+                else
+                    cout << " " << (i * 3 + j + 1) << " ";
+                if (j < 2) cout << "|";
+            }
+            cout << "\n";
             if (i < 2) cout << "---|---|---\n";
         }
         cout << "\n";
     }
 
-    bool make_move(int position) {
-        if (position < 1 || position > 9) {
-            cout << "Invalid position. Choose 1-9.\n";
+    bool make_move(int pos) {
+        if (pos < 1 || pos > 9) {
+            cout << "Invalid input. Choose a number between 1 and 9.\n";
             return false;
         }
-        int row = (position - 1) / 3;
-        int col = (position - 1) % 3;
-        if (board[row][col] == 'X' || board[row][col] == 'O') {
+        int row = (pos - 1) / 3;
+        int col = (pos - 1) % 3;
+        if (board[row][col] != 0) {
             cout << "Cell already taken. Try again.\n";
             return false;
         }
@@ -44,33 +53,40 @@ public:
     }
 
     void switch_player() {
-        current_player = (current_player == 'X') ? 'O' : 'X';
+        current_player = 3 - current_player; // 1 -> 2, 2 -> 1
     }
 
     bool check_win() {
         for (int i = 0; i < 3; ++i) {
-            if (board[i][0] == current_player && board[i][1] == current_player && board[i][2] == current_player)
+            if (board[i][0] == current_player &&
+                board[i][1] == current_player &&
+                board[i][2] == current_player)
                 return true;
-            if (board[0][i] == current_player && board[1][i] == current_player && board[2][i] == current_player)
+            if (board[0][i] == current_player &&
+                board[1][i] == current_player &&
+                board[2][i] == current_player)
                 return true;
         }
-        if (board[0][0] == current_player && board[1][1] == current_player && board[2][2] == current_player)
+        if (board[0][0] == current_player &&
+            board[1][1] == current_player &&
+            board[2][2] == current_player)
             return true;
-        if (board[0][2] == current_player && board[1][1] == current_player && board[2][0] == current_player)
+        if (board[0][2] == current_player &&
+            board[1][1] == current_player &&
+            board[2][0] == current_player)
             return true;
-
         return false;
     }
 
     bool is_draw() {
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
-                if (board[i][j] != 'X' && board[i][j] != 'O')
+                if (board[i][j] == 0)
                     return false;
         return true;
     }
 
-    char get_current_player() const {
+    int get_current_player() const {
         return current_player;
     }
 };
@@ -81,7 +97,9 @@ int main() {
 
     while (true) {
         game.display_board();
-        cout << "Player " << game.get_current_player() << ", enter your move (1-9): ";
+        cout << "Player " << game.get_current_player() << " (" 
+             << (game.get_current_player() == 1 ? 'X' : 'O') 
+             << "), enter your move (1-9): ";
         cin >> pos;
 
         if (!game.make_move(pos))
